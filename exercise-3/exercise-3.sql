0af1268f413b022b8d3e723348593789fd4cf180
CarolynnFleming <cfcatpink@gmail.com>
2022-03-25 09:49:12 -0700

    exercise three changed city,city_id to country_id

--- exercise-3/exercise-3.sql
+++ exercise-3/exercise-3.sql
@@ -6,7 +6,7 @@ SELECT
   AVG(amount)
 FROM
   country
-  INNER JOIN city ON country.country_id = city.city_id
+  INNER JOIN city ON country.country_id = city.country_id
   INNER JOIN address ON city.city_id = address.city_id
   INNER JOIN customer ON address.address_id = customer.address_id
   INNER JOIN payment ON customer.customer_id = payment.customer_id
